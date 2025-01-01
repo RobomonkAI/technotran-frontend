5f23b81c76698a7b851fc0f884e7bdf0572f4566
CVS CHARAN <cvs@CVSs-MacBook-Pro.local>
2025-01-02 01:36:04 +0530

    MOU Page Final

--- src/app/mou/page.tsx
+++ src/app/mou/page.tsx
@@ -8,6 +8,7 @@ import WhatsAppBtn from "@/components/WhatsAppBtn";
 import PhoneBtn from "@/components/PhoneBtn";
 import FooterMain from "@/sections/FooterMain";
 import { motion } from "framer-motion";
+import { mouData } from "./utils/helpers";
 
 const MOUPage = () => {
   const headingVariants = {
@@ -29,6 +30,30 @@ const MOUPage = () => {
       y: 0,
       transition: {
         duration: 0.3,
+        ease: "easeInOut",
+      },
+    },
+  };
+
+  const imageVariant = {
+    hidden: { opacity: 0, x: -50 }, // Start off-screen to the left
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.8,
+        ease: "easeInOut",
+      },
+    },
+  };
+
+  const contentVariant = {
+    hidden: { opacity: 0, x: 50 }, // Start off-screen to the right
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.8,
         ease: "easeInOut",
       },
     },
@@ -72,6 +97,78 @@ const MOUPage = () => {
           </motion.h3>
         </div>
       </section>
+      <section id="MOU Content Section" className={styles.gallerySection}>
+        <motion.h2
+          initial="hidden"
+          whileInView="visible"
+          variants={headingVariants}
+          viewport={{ once: false, amount: 0.4 }}
+          className={`${styles.subTitle} cinzel-text`}
+        >
+          Mou with Institutions
+        </motion.h2>
+        <div className={styles.galleryContainer}>
+          {mouData.map((item, index) => (
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              variants={cardVariants}
+              viewport={{ once: false, amount: 0.3 }}
+              key={item.id}
+              className={styles.galleryItem}
+            >
+              <Image
+                src={item.imgSrc}
+                height={200}
+                width={200}
+                priority
+                alt={`MOU Img${index}`}
+                className={styles.galleryImage}
+              />
+              <div className={styles.galleryCaption}>
+                <h2 className="quicksand-text">{item.name}</h2>
+              </div>
+            </motion.div>
+          ))}
+        </div>
+      </section>
+      <motion.section
+        id="MOU Commitment Section"
+        className={styles.mouContainer}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: false, amount: 0.5 }} // Trigger animation when section is in the viewport
+      >
+        <div className={styles.mouSubContainer}>
+          <motion.div
+            className={styles.mouImgContainer}
+            variants={imageVariant}
+          >
+            <Image
+              src="https://github.com/CVSCharan/Technotran_Assets/blob/main/commitment.png?raw=true"
+              alt="MOU Commitment Img"
+              className={styles.mouImg}
+              height={700}
+              width={700}
+              priority
+            />
+          </motion.div>
+          <motion.div
+            className={styles.mouContentContainer}
+            variants={contentVariant}
+          >
+            <h2 className={`${styles.mouTitle} cinzel-text`}>Commitment</h2>
+            <h3 className={`${styles.mouDesc} quicksand-text`}>
+              {`Technotran is committed to upholding the terms of this Memorandum
+              of Understanding and working collaboratively with partner
+              organizations to achieve shared goals and objectives. Through
+              sustained cooperation and mutual support, we aim to make
+              significant contributions to technology education, research, and
+              innovation for the benefit of society.`}
+            </h3>
+          </motion.div>
+        </div>
+      </motion.section>
       <FooterMain />
       <WhatsAppBtn />
       <PhoneBtn />
---
+++ src/app/mou/utils/types.tsx
@@ -0,0 +1,5 @@
+export interface MouDataItem {
+  id: number;
+  name: string;
+  imgSrc: string;
+}
