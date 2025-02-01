e2aa14bd62156b95704bc3136a5e63908dc9f9ed
CVS CHARAN <cvs@CVSs-MacBook-Pro.local>
2025-02-01 17:28:21 +0530

    added the mobile responsiveness

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -7,7 +7,6 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import WhatsAppBtn from "@/components/WhatsAppBtn";
 import PhoneBtn from "@/components/PhoneBtn";
-import CustomGoogleMap from "../../components/CumstomGMaps";
 import PhoneIcon from "@mui/icons-material/Phone";
 import EmailIcon from "@mui/icons-material/Email";
 import FooterMain from "@/sections/FooterMain";
@@ -67,6 +66,14 @@ const ContactUsPage = () => {
               {`We're here to assist you and answer any questions you may have. Feel
             free to reach out to us.`}
             </motion.h2>
+            <Image
+              alt="Contact Us Banner Img"
+              src="/images/contact-us-banner-img.jpg"
+              height={500}
+              width={500}
+              priority
+              className={styles.bannerImage}
+            />
           </div>
         </section>
         <motion.section
@@ -78,53 +85,36 @@ const ContactUsPage = () => {
           className={styles.contactSection}
         >
           <div className={styles.contactContainer}>
-            {/* Left Section */}
-            <div className={styles.leftSection}>
-              <Image
-                alt="Contact Us Banner Img"
-                src="/images/contact-us-banner-img.jpg"
-                height={500}
-                width={500}
-                priority
-                className={styles.bannerImage}
-              />
-
-              <CustomGoogleMap />
-            </div>
-
-            {/* Right Section */}
-            <div className={styles.rightSection}>
-              <Image
-                src="/images/Technotran-logo.png"
-                alt="Contact Us Logo"
-                height={200}
-                width={200}
-                priority
-                className={styles.logoImage}
-              />
-              <div className={styles.contactDetails}>
-                <div className={styles.contactItem}>
-                  <h2>Address</h2>
-                  <p>Technotran office, Near more supermarket,</p>
-                  <p>beside Canara bank, Magunta layout, Nellore.</p>
+            <Image
+              src="/images/Technotran-logo.png"
+              alt="Contact Us Logo"
+              height={200}
+              width={200}
+              priority
+              className={styles.logoImage}
+            />
+            <div className={styles.contactDetails}>
+              <div className={styles.contactItem}>
+                <h2>Address</h2>
+                <p>Technotran office, Near more supermarket,</p>
+                <p>beside Canara bank, Magunta layout, Nellore.</p>
+              </div>
+              <div className={styles.contactItem}>
+                <h2>Email</h2>
+                <div className={styles.contactInfo}>
+                  <EmailIcon className={styles.icon} />
+                  <p>info@technotran.in</p>
                 </div>
-                <div className={styles.contactItem}>
-                  <h2>Email</h2>
-                  <div className={styles.contactInfo}>
-                    <EmailIcon className={styles.icon} />
-                    <p>info@technotran.in</p>
-                  </div>
+              </div>
+              <div className={styles.contactItem}>
+                <h2>Contact Number</h2>
+                <div className={styles.contactInfo}>
+                  <PhoneIcon className={styles.icon} />
+                  <p>+91 9000 326 936</p>
                 </div>
-                <div className={styles.contactItem}>
-                  <h2>Contact Number</h2>
-                  <div className={styles.contactInfo}>
-                    <PhoneIcon className={styles.icon} />
-                    <p>+91 9000 326 936</p>
-                  </div>
-                  <div className={styles.contactInfo}>
-                    <PhoneIcon className={styles.icon} />
-                    <p>+91 9100 103 806</p>
-                  </div>
+                <div className={styles.contactInfo}>
+                  <PhoneIcon className={styles.icon} />
+                  <p>+91 9100 103 806</p>
                 </div>
               </div>
             </div>
